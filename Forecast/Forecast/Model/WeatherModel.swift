5e54b829bfcae5e6449aa1596c6e9a3d1dd7178d
jboro <joseph@videomyjob.com>
2020-09-14 13:45:22 +0800

    - added screenshot to readme - ui fixes

--- Forecast/Forecast/Model/WeatherModel.swift
+++ Forecast/Forecast/Model/WeatherModel.swift
@@ -19,7 +19,7 @@ struct WeatherModel{
     
     var temperatureInCelsius: String{
         let convertedKtoCTemp = temperature - 273.15
-        return String(format: "%.1f C", convertedKtoCTemp)
+        return String(format: "%.1f", convertedKtoCTemp)
     }
     
     func getWeatherDescription(weatherId: Int) -> String {
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ForecastiOS
 Simple weather app for iOS that uses OpenWeatherMap api
+![Screenshot](/forecastscreenshot.png)
